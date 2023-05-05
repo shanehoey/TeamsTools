13160133485cd7ff8ed8de36a6c61155a1741cae
Shane Hoey <5053990+shanehoey@users.noreply.github.com>
2023-05-05 12:28:31 +1000

    Delete scripts directory

--- scripts/profile.ps1
+++
@@ -1,5 +0,0 @@
-Write-Verbose "TeamsTools" -verbose
-if ($PSVersionTable.platform -eq 'Unix'){ write-warning "Have you configured wsman ? ie install-module PSWSMan & install-wsman" }
-if ($PSVersionTable.platform -eq 'Unix'){if (!(get-module PSWSMAN -ListAvailable)) {install-module -name pswsman -scope AllUsers -force ; install-wsman}}
-
-
