ff1697a248c606f6d8b1bd6888eb4bcf5261fc58
Shane Hoey <5053990+shanehoey@users.noreply.github.com>
2023-05-05 12:29:08 +1000

    Delete _scripts directory

--- _scripts/_build_ghcr.ps1
+++
@@ -1,9 +0,0 @@
-exit
-
-set-location $env:USERPROFILE\code\shanehoey\TeamsTools
-
-docker build -t ghcr.io/shanehoey/teamstools:latest -f Dockerfile .
-
-docker login ghcr.io -u shanehoey
-
-docker push ghcr.io/shanehoey/teamstools:latest
--- _scripts/_build_local.ps1
+++
@@ -1,7 +0,0 @@
-exit
-
-##IMPORTANT - Docker hub should be built from docker hub
-
-set-location $env:USERPROFILE\github\shanehoey\TeamsTools
-
-docker build -t shanehoey/teamstools:latest -f Dockerfile .
