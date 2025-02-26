bd6439b7508fb7cea27a03cc5626587dd0e39080
Shane Hoey <5053990+shanehoey@users.noreply.github.com>
2025-02-26 19:17:02 +1000

    modified:   teamsToolsReset/public/remove-teamsToolsConfig.ps1 	modified:   teamsToolsReset/public/remove-teamsToolsOnlinePSTNGateway.ps1

--- teamsToolsReset/public/remove-teamsToolsConfig.ps1
+++ teamsToolsReset/public/remove-teamsToolsConfig.ps1
@@ -1,13 +1,43 @@
-function remove-teamsToolsConfig {
+ <#
+.SYNOPSIS
+Removes Teams configuration for a specified domain.
 
-    [CmdletBinding(SupportsShouldProcesss, ConfirmImpact = "HIGH")]
+.DESCRIPTION
+The remove-teamsToolsConfig function removes all Teams configuration settings for a specified domain. 
+It supports the -clean switch to remove all configurations and the -skipusers switch to skip user removal.
+
+.PARAMETER clean
+Optional switch to indicate if all Teams configuration should be removed.
+
+.PARAMETER domainname
+Mandatory parameter specifying the domain name for which Teams configuration should be removed.
+Mandatory parameter specifying the domain name for which Teams configuration should be removed.
+
+.PARAMETER skipusers
+Optional switch to indicate if user removal should be skipped.
+
+.EXAMPLE
+remove-teamsToolsConfig -domainname "shanehoey.dev" -clean
+
+This command removes all Teams configuration for the tenant. "shanehoey.dev".
+
+.EXAMPLE
+remove-teamsToolsConfig -domainname "shanehoey.dev" -clean -skipusers
+
+This command removes all Teams configuration for the domain "shanehoey.dev" but skips user removal.
+
+.NOTES
+Ensure you have the necessary permissions to run this script and modify Teams configurations.
+#>
+function remove-teamsToolsConfig {
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact = "HIGH")]
     param(
         [switch]$clean = $false,
         [Parameter(Mandatory = $true)]
         [string]$domainname,
         [switch]$skipusers 
     )
- 
+
     if (get-cstenant | Where-Object { $_.SipDomain -eq $domainname }) {
 
         If ($PSBoundParameters.ContainsKey('clean')) { 
--- teamsToolsReset/public/remove-teamsToolsOnlinePSTNGateway.ps1
+++ teamsToolsReset/public/remove-teamsToolsOnlinePSTNGateway.ps1
@@ -1,4 +1,25 @@
+<#
+.SYNOPSIS
+Removes all Online PSTN Gateways in Microsoft Teams.
+
+.DESCRIPTION
+The remove-teamsToolsOnlinePSTNGateway function retrieves all Online PSTN Gateways, sorts them in descending order by ProxySBC, and removes each one after confirming with the user. It logs the removal of each gateway and handles any errors that occur during the process.
+
+.PARAMETERS
+This function does not take any parameters.
+
+.EXAMPLES
+.EXAMPLE
+remove-teamsToolsOnlinePSTNGateway
+
+This example removes all Online PSTN Gateways after user confirmation.
+
+.NOTES
+This function requires the Teams PowerShell module to be installed and connected to the appropriate Teams environment.
+
+#>
 function remove-teamsToolsOnlinePSTNGateway {
+
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "HIGH")]
     param()
 
